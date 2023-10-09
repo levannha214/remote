3e74e5d99d13c6157cc270c35eae2610e766542c
LeVanNha214 <125424041+LeVanNha214@users.noreply.github.com>
2023-10-09 16:12:37 +0700

    {message}

--- components/Remote.js
+++ components/Remote.js
@@ -1,15 +1,10 @@
-import { useEffect,useRef,useState } from 'react';
-
+import { useEffect,useRef } from 'react';
+import { Icon } from '@rneui/themed';
 import { View, Text, StyleSheet, Image, Animated } from 'react-native';
-const button_arrow = require("./image/button_arrow.png");
-const button_2 = require("./image/button_2.png");
 const list_button = require("./image/litst_button.png");
-const button_left = require("./image/next_left.png");
-const button_right = require("./image/next_right.png");
 const remote = require('./image/remote.png');
-
 const Remote = ({ check }) => {
-    const [remoteNext, setRemoteNext] = useState(30);
+   
   
     const styleRemote = [{
         b1: <Image source={remote} style={
@@ -32,11 +27,21 @@ const Remote = ({ check }) => {
                marginTop:230, with: "100%", height: 25, flexDirection: 'row',
                 justifyContent: 'space-between', paddingLeft: 7, paddingRight: 7,
             }} >
-                <View style={{ mixBlendMode: 'overlay', backgroundColor: "rgba(255,255,255,0.5)", width: 23, height: 23, borderColor: "red", borderWidth: 1, borderRadius: 50 }}>
-                    <Image source={button_left} style={{ width: 20, height: 20, borderRadius: 50 }}></Image>
+                <View style={{ mixBlendMode: 'overlay', backgroundColor: "rgba(255,255,255,1)",justifyContent:'center',alignItems:"center", width: 23, height: 23, borderColor: "red", borderWidth: 1, borderRadius: 50 }}>
+      <Icon
+        size={10}
+        name='backward'
+        type='font-awesome'
+        color='#000'
+        />
                 </View>
-                <View style={{ mixBlendMode: 'overlay', backgroundColor: "rgba(255,255,255,0.5)", width: 23, height: 23, borderColor: "red", borderWidth: 1, borderRadius: 50 }}>
-                    <Image source={button_right} style={{ width: 20, height: 20, borderRadius: 50 }}></Image>
+                <View style={{ mixBlendMode: 'overlay', backgroundColor: "rgba(255,255,255,1)",justifyContent:'center',alignItems:"center", width: 23, height: 23, borderColor: "red", borderWidth: 1, borderRadius: 50 }}>
+                   <Icon
+        size={10}
+        name='forward'
+        type='font-awesome'
+        color='#000'
+        />
                 </View>
             </View>
         </View>
@@ -50,9 +55,9 @@ const Remote = ({ check }) => {
         } ></Image>,
         a1: <View style={{ width: '100%', height: "100%", backgroundColor: "rgba(000,000,000,0.75)", position: "absolute", borderRadius: 23 }}>
             <View style={{
-                with: "100%", height: 110, marginTop: 258, backgroundColor: "rgba(255,255,255,0.5)", borderColor: "red", borderWidth: 1,
+                with: "100%", height: 112, marginTop: 257, backgroundColor: "rgba(255,255,255,0.5)", borderColor: "red", borderWidth: 1,
             }} >
-                <Image source={list_button} style={{ width: 88, height: 108 }}></Image>
+                <Image source={list_button} style={{ width: 88, height: 109 }}></Image>
             </View>
         </View>
     }, {
@@ -64,9 +69,25 @@ const Remote = ({ check }) => {
         } ></Image>,
         a1: <View style={{  width: '100%', height: "100%", backgroundColor: "rgba(000,000,000,0.75)", position: "absolute", borderRadius: 23 }}>
             <View style={{
-                width: 20, height: 53, marginLeft: 62, borderRadius: 50, marginTop: 175, backgroundColor: "rgba(255,255,255,0.5)", borderColor: "red", borderWidth: 1,
+                width: 20, height: 53, marginLeft: 62, borderRadius: 50, marginTop: 175, backgroundColor: "rgba(255,255,255,1)", borderColor: "red", borderWidth: 1,
             }} >
-                <Image source={button_2} style={{ width: 18, height: 51, borderRadius: 53 }}></Image>
+                <View style={{ flexDirection: 'column' }}>
+                    <View style={{width:16,height:16,borderRadius:50,backgroundColor:'rgba(000,000,000,0.1)',justifyContent:"center",alignItems:'center',marginTop:1,marginLeft:1}}><Icon
+                        
+                    size={10}
+        name='chevron-up'
+        type='font-awesome'
+        color='#000'
+                    /></View>
+                    <View style={{justifyContent:'center',alignItems:"center",height:15}}><Text style={{fontSize:7,fontWeight:900}}>CH</Text></View>
+                </View>
+                <View style={{ width: 17, height: 17, borderRadius: 50, backgroundColor: 'rgba(000,000,000,0.1)', marginTop: 1, justifyContent: "center", alignItems: 'center' }}>
+                    <Icon
+                    size={10}
+        name='chevron-down'
+        type='font-awesome'
+        color='#000'
+                    /></View> 
             </View>
         </View>
     }, {
@@ -78,9 +99,14 @@ const Remote = ({ check }) => {
         } ></Image>,
         a1: <View style={{ width: '100%', height: "100%", backgroundColor: "rgba(000,000,000,0.75)", position: "absolute", borderRadius: 23 }}>
             <View style={{
-                width: 30, height: 20, borderRadius: 50, marginTop: 146, backgroundColor: "rgba(255,255,255,0.5)", borderColor: "red", borderWidth: 1,
+                width: 28, height: 18, borderRadius: 50, marginTop: 146, backgroundColor: "rgba(255,255,255,1)", borderColor: "red", borderWidth: 1,
             }} >
-                <Image source={button_arrow} style={{ width: 27, height: 17, borderRadius: 53 }}></Image>
+                 <View style={{justifyContent:"center",alignItems:"center",paddingTop:2}}><Icon
+                    size={10}
+        name='arrow-left'
+        type='font-awesome'
+        color='#000'
+                    /></View>
             </View>
         </View>
     },
